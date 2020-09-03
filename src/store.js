a152cf1758081abe9dd0a9f7f8f61201e4ee1bb9
doakcovington <doakcovington@protonmail.com>
2020-09-03 14:36:45 -0700

    Built out store and tutor reducer

--- src/store.js
+++ src/store.js
@@ -1,8 +1,11 @@
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 
+//reducers
+import tutor from './reducers/tutor';
+
 const reducer = combineReducers({
-  
+  tutor
 });
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
