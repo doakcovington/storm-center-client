c7474f5442a8fe526efa66980de9099630fa4a47
doakcovington <doakcovington@protonmail.com>
2020-09-04 11:41:25 -0700

    Created events container

---
+++ src/containers/EventsContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class EventsContainer extends React.Component {
+
+  render() {
+    return (
+      <div>Hello from EventsContainer</div>
+    )
+  }
+}
+
+export default EventsContainer;
