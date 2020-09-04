420752ec8d6545717fadab5896090ffc45799c47
doakcovington <doakcovington@protonmail.com>
2020-09-04 14:13:05 -0700

    built out handleChange function

--- src/components/CreateEvent.js
+++ src/components/CreateEvent.js
@@ -1,14 +1,19 @@
 import React from 'react';
 
 class CreateEvent extends React.Component {
-
   state = {
     name: '',
     description: '',
     date: '',
     location: '',
     spaces: '',
-    tutor: ''
+    tutor: '1' //sets tutor to first in select option
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value //sets the state for each key in state object [are used for keys with '.' in name]
+    })
   }
 
   render() {
@@ -16,17 +21,17 @@ class CreateEvent extends React.Component {
       <div>
         <form>
           <label>Event Name:</label>
-          <input type='text' placeholder='Name' value={this.state.name}/><br></br>
+          <input type='text' placeholder='Name' value={this.state.name} name='name' onChange={this.handleChange}/><br></br>
           <label>Event Description:</label>
-          <input type='text' placeholder='Description' value={this.state.description}/><br></br>
+          <input type='text' placeholder='Description' value={this.state.description} name='description' onChange={this.handleChange}/><br></br>
           <label>Event Date:</label>
-          <input type='date' value={this.state.date}/><br></br>
+          <input type='date' name='date' value={this.state.date}/><br></br>
           <label>Event Location/Link:</label>
-          <input type='text' placeholder='url...' value={this.state.location}/><br></br>
+          <input type='text' placeholder='url...' value={this.state.location} name='location' onChange={this.handleChange}/><br></br>
           <label>Event Spaces:</label>
-          <input type='number' placeholder='5' value={this.state.number}/><br></br>
+          <input type='number' placeholder='5' value={this.state.number} name='spaces' onChange={this.handleChange}/><br></br>
           <label>Event Tutor:</label>
-          <select id='tutors' name='tutors' value={this.state.tutor}>
+          <select id='tutor' name='tutor' value={this.state.tutor} onChange={this.handleChange}>
             <option value='1'>Doak</option>
             <option value='2'>Heidi</option>
           </select><br></br>
