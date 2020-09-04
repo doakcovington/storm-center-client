1bb98c07833977b2bb13747cfb30403fbbba3a07
doakcovington <doakcovington@protonmail.com>
2020-09-04 12:50:52 -0700

    Formatting

--- src/components/Events.js
+++ src/components/Events.js
@@ -3,7 +3,9 @@ import React from 'react'
 const Events = (props) => {
   return (
     <div>
-     {props.events.map(event => <li key={event.id}>{event.attributes.name} - {event.attributes.description} - Spaces: {event.attributes.spaces} Tutor: {event.attributes.tutor.name}</li>)}
+     {props.events.map(event => <li key={event.id}>{event.attributes.name} - 
+     {event.attributes.description} - Spaces: {event.attributes.spaces} 
+     Tutor: {event.attributes.tutor.name}</li>)}
     </div>
   )
 }
