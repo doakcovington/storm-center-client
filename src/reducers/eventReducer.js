b872982b93fa8c94b33ccfc1feb37823ba122862
doakcovington <doakcovington@protonmail.com>
2020-09-04 12:11:37 -0700

    Built out eventReducer

--- src/reducers/eventReducer.js
+++ src/reducers/eventReducer.js
@@ -1,4 +1,8 @@
 export default function eventReducer(state = {events: []}, action) { //default state is an empty array of events
-  
-  return state;
+  switch (action.type) {
+    case 'FETCH_EVENTS':
+      return {events: action.payload}
+    default:
+      return state
+  }
 }
