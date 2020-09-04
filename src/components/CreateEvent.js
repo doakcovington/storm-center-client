fc5229a46704380b1456a06ff5c9e8cca63c2efd
doakcovington <doakcovington@protonmail.com>
2020-09-04 13:42:56 -0700

    Built out createEvents form

--- src/components/CreateEvent.js
+++ src/components/CreateEvent.js
@@ -4,7 +4,24 @@ class CreateEvent extends React.Component {
   render() {
     return (
       <div>
-        <form></form>
+        <form>
+          <label>Event Name:</label>
+          <input type='text' placeholder='Name'/>
+          <label>Event Description:</label>
+          <input type='text' placeholder='Description'/>
+          <label>Event Date:</label>
+          <input type='date' />
+          <label>Event Location/Link:</label>
+          <input type='text' placeholder='url...'/>
+          <label>Event Spaces:</label>
+          <input type='number' placeholder='5'/>
+          <label>Event Tutor:</label>
+          <select id='tutors' name='tutors'>
+            <option value='1'>Doak</option>
+            <option value='2'>Heidi</option>
+          </select>
+          <button>Submit</button>
+        </form>
       </div>
     )
   }
