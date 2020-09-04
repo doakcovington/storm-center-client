3ebe30df3cd9824a7559e679615b170c35a9153f
doakcovington <doakcovington@protonmail.com>
2020-09-04 13:48:10 -0700

    Controlled form

--- src/components/CreateEvent.js
+++ src/components/CreateEvent.js
@@ -1,25 +1,35 @@
 import React from 'react';
 
 class CreateEvent extends React.Component {
+
+  state = {
+    name: '',
+    description: '',
+    date: '',
+    location: '',
+    spaces: '',
+    tutor: ''
+  }
+
   render() {
     return (
       <div>
         <form>
           <label>Event Name:</label>
-          <input type='text' placeholder='Name'/>
+          <input type='text' placeholder='Name' value={this.state.name}/><br></br>
           <label>Event Description:</label>
-          <input type='text' placeholder='Description'/>
+          <input type='text' placeholder='Description' value={this.state.description}/><br></br>
           <label>Event Date:</label>
-          <input type='date' />
+          <input type='date' value={this.state.date}/><br></br>
           <label>Event Location/Link:</label>
-          <input type='text' placeholder='url...'/>
+          <input type='text' placeholder='url...' value={this.state.location}/><br></br>
           <label>Event Spaces:</label>
-          <input type='number' placeholder='5'/>
+          <input type='number' placeholder='5' value={this.state.number}/><br></br>
           <label>Event Tutor:</label>
-          <select id='tutors' name='tutors'>
+          <select id='tutors' name='tutors' value={this.state.tutor}>
             <option value='1'>Doak</option>
             <option value='2'>Heidi</option>
-          </select>
+          </select><br></br>
           <button>Submit</button>
         </form>
       </div>
