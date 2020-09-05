15e7e88beed747d7f7fdc9af1254f11e9cef869b
doakcovington <doakcovington@protonmail.com>
2020-09-05 11:25:14 -0700

    Built out addEvent

--- src/actions/addEvent.js
+++ src/actions/addEvent.js
@@ -3,10 +3,12 @@ export const addEvent = (formData) => {
     fetch('http://localhost:5000/api/v1/events', {
       headers: {
         'Content-Type': 'application/json',
-
       },
       method: 'POST',
       body: JSON.stringify(formData)
     })
+    .then(response => response.json())
+    .then(event => dispatch({type: 'ADD_EVENT', payload: event.data}))
   }
 }
+
