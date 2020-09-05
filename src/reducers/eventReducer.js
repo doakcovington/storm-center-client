1eaf6a079c8561ea947fe07508c72cb4313df50b
doakcovington <doakcovington@protonmail.com>
2020-09-05 12:30:06 -0700

    Event is added to state

--- src/reducers/eventReducer.js
+++ src/reducers/eventReducer.js
@@ -2,6 +2,11 @@ export default function eventReducer(state = {events: []}, action) { //default s
   switch (action.type) {
     case 'FETCH_EVENTS':
       return {events: action.payload}
+    case 'ADD_EVENT': //DETAILS DETAILS DETAILS (Had 'ADD_EVENTS'. Removed the 'S' to fix the page refresh bug)
+      return {
+        ...state, 
+        events: [...state.events, action.payload]
+      } //adds the event to the state
     default:
       return state
   }
