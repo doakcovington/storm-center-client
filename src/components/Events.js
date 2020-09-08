26dd0cec4a0429a12d4467da0cc415d91acf4fa9
doakcovington <doakcovington@protonmail.com>
2020-09-08 13:34:42 -0700

    Created ShowEvent file

--- src/components/Events.js
+++ src/components/Events.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import ShowEvent from './ShowEvent';
 
 const Events = (props) => {
   return (
---
+++ src/components/ShowEvent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ShowEvent = (props) => {
+  return (
+    <div>
+     
+    </div>
+  )
+}
+
+export default ShowEvent
