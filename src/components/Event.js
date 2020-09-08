f7b3b1435d4c1102570aea0e74632bb9f46dd4e2
doakcovington <doakcovington@protonmail.com>
2020-09-08 13:45:16 -0700

    Built out Event Component

--- src/components/Event.js
+++ src/components/Event.js
@@ -3,7 +3,12 @@ import React from 'react';
 const Event = (props) => {
   return (
     <div>
-     
+     <h2>{props.event.attributes.name}</h2>
+     <h3>{props.event.attributes.date}</h3>
+     <p>{props.event.attributes.description}</p>
+     <p>Tutor: {props.event.attributes.tutor.name}</p>
+     <p>Location: {props.event.attributes.location}</p>
+     <p>Spaces Left: {props.event.attributes.spaces}</p>
     </div>
   )
 }
