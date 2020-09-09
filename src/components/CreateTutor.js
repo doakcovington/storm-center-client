01dcc92525fe9ae15459fb6409953e6bb793883a
doakcovington <doakcovington@protonmail.com>
2020-09-09 16:19:17 -0700

    Built out form

--- src/components/CreateTutor.js
+++ src/components/CreateTutor.js
@@ -5,7 +5,18 @@ class CreateTutor extends React.Component {
 
   render() {
     return (
-      <div>Howdy from CreateTutor</div>
+      <div>
+        <form>
+          <label>Name:</label>
+          <input type='text' placeholder='Name' value={this.state.name} name='name' onChange={this.handleChange}/><br></br>
+          <label>Speciality:</label>
+          <input type='text' placeholder='Speciality' value={this.state.speciality} name='speciality' onChange={this.handleChange}/><br></br>
+          <label>Phone Number:</label>
+          <input type='text' placeholder='555-555-5555' value={this.state.phonenumber} name='phonenumber' onChange={this.handleChange}/><br></br>
+          <label>Email:</label>
+          <input type='text' placeholder='tutor@email.com' value={this.state.email} name='email' onChange={this.handleChange}/><br></br>
+        </form>
+      </div>
     )
   }
 }
