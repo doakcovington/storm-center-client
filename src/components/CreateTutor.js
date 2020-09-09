b4e92f878ee3452332cc67d0f8d29c9eee5ab706
doakcovington <doakcovington@protonmail.com>
2020-09-09 11:52:17 -0700

    Created CreateTutor

---
+++ src/components/CreateTutor.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class CreateTutor extends React.Component {
+
+  render() {
+    return (
+      <div>Howdy from CreateTutor</div>
+    )
+  }
+}
+
+export default CreateTutor;
