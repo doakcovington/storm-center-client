31523937266b66f31060314d275a87b7e44eba39
doakcovington <doakcovington@protonmail.com>
2020-09-09 18:47:46 -0700

    built out defaultState

--- src/reducers/eventReducer.js
+++ src/reducers/eventReducer.js
@@ -1,7 +1,11 @@
-export default function eventReducer(state = {events: []}, action) { //default state is an empty array of events
+const defaultState = {
+  events: []
+}
+
+export default function eventReducer(state = defaultState, action) { //default state is an empty array of events
   switch (action.type) {
     case 'FETCH_EVENTS':
-      return {events: action.payload}
+      return {...state, events: action.payload}
     case 'ADD_EVENT': //DETAILS DETAILS DETAILS (Had 'ADD_EVENTS'. Removed the 'S' to fix the page refresh bug)
       return {
         ...state, 
