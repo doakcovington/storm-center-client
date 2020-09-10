ce73535175c8eaa223e15f959ed767cfe768cda9
doakcovington <doakcovington@protonmail.com>
2020-09-10 16:48:28 -0700

    Built out handleDelete

--- src/components/Tutors.js
+++ src/components/Tutors.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
 const Tutors = (props) => {
-  console.log(props)
+  
+  const handleDelete = (tutorData) => {
+    props.deleteTutor(tutorData)
+  }
+
   return (
     <div>
-      {props.tutors.map(tutor => <div key={tutor.id}>{tutor.attributes.name}</div>)}
+      {
+        props.tutors.map(tutor => 
+          <div key={tutor.id}>{tutor.attributes.name} <button onClick={() => handleDelete(tutor.id)}>Delete</button> 
+          </div>)
+      }
     </div>
   )
 }
