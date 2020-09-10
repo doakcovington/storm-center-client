2da1d06f7ba0d9a0ce39da1e5f537c88397dcbe3
doakcovington <doakcovington@protonmail.com>
2020-09-10 15:57:15 -0700

    Added handleDelete

--- src/components/Events.js
+++ src/components/Events.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import Event from './Event';
+import {connect} from 'react-redux';
+import {deleteEvent} from '../actions/deleteEvent';
 
 const Events = (props) => {
-  console.log(props)
+  
+  const handleDelete = (eventData) => {
+    props.deleteEvent(eventData)
+  }
+
   return (
     <div>
-     {props.events.map(event => <div key={event.id}><Event event={event}/> </div>)}
+     {
+      props.events.map(event => <div key={event.id}><Event event={event}/>
+      <button onClick={() => handleDelete(event.id)}>Delete</button>
+      </div>)
+     }
     </div>
   )
 }
 
-export default Events;
+export default connect(null, {deleteEvent})(Events);
