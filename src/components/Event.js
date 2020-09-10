a09e64aa1028c914d31928c536a8cfc3c00eeab9
doakcovington <doakcovington@protonmail.com>
2020-09-10 15:56:20 -0700

    moved delete function to Events component

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,12 +1,6 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import {deleteEvent} from '../actions/deleteEvent';
 
 const Event = (props) => {
-
-  const handleDelete = () => {
-
-  }
 
   return (
     <div key={props.event.id}>
@@ -16,9 +10,8 @@ const Event = (props) => {
      <p>Tutor: {props.event.attributes.tutor.name}</p>
      <p>Location: {props.event.attributes.location}</p>
      <p>Spaces Left: {props.event.attributes.spaces}</p>
-     <button onClick={handleDelete}>Delete</button>
     </div>
   )
 }
 
-export default connect(null, {deleteEvent})(Event);
+export default Event;
