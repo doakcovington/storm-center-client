af6565cae64d45b11b2ff9e1561b14e27712bdf2
doakcovington <doakcovington@protonmail.com>
2020-09-10 11:55:30 -0700

    Adde connect

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,16 +1,24 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {deleteEvent} from '../actions/deleteEvent';
 
 const Event = (props) => {
+
+  const handleDelete = () => {
+
+  }
+
   return (
-    <div>
+    <div key={props.event.id}>
      <h2>{props.event.attributes.name}</h2>
      <h3>{props.event.attributes.date}</h3>
      <p>{props.event.attributes.description}</p>
      <p>Tutor: {props.event.attributes.tutor.name}</p>
      <p>Location: {props.event.attributes.location}</p>
      <p>Spaces Left: {props.event.attributes.spaces}</p>
+     <button onClick={handleDelete}>Delete</button>
     </div>
   )
 }
 
-export default Event
+export default connect(null, {deleteEvent})(Event);
