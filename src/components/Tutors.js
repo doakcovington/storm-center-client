4a1118f8b74f2b87abfb3ba61873120a5d952165
doakcovington <doakcovington@protonmail.com>
2020-09-09 19:06:56 -0700

    Created Tutors component

---
+++ src/components/Tutors.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Tutors = (props) => {
+  console.log(props)
+  return (
+    <div>
+      Hello from Tutors component
+    </div>
+  )
+}
+
+export default Tutors;
