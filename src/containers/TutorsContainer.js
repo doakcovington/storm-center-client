59afb61053c446e6748404a6009f38e241557d9e
doakcovington <doakcovington@protonmail.com>
2020-09-09 19:23:35 -0700

    Built out TutorsContainer

--- src/containers/TutorsContainer.js
+++ src/containers/TutorsContainer.js
@@ -1,15 +1,34 @@
 import React from 'react';
 import CreateTutor from '../components/CreateTutor';
+import Tutors from '../components/Tutors';
+import {connect} from 'react-redux';
+import {getTutors} from '../actions/getTutors';
 
 class TutorsContainer extends React.Component {
+
+  componentDidMount() {
+    this.props.getTutors();
+  }
+
   render() {
     return (
       <div>
         Howdy for the Tutors Container!
+        <Tutors tutors={this.props.tutors}/>
         <CreateTutor />
       </div>
     );
   }
 }
 
-export default TutorsContainer;
+const mapStateToProps = state => {
+  return {
+    tutors: state.tutorReducer.tutors
+  }
+}
+
+const mapDispatchToProps = {
+  getTutors: getTutors
+}
+ 
+export default connect(mapStateToProps, mapDispatchToProps)(TutorsContainer);
