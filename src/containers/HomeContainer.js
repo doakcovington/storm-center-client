34a82f94b5bd4ec0d34c04d56cc8241e62d6a977
doakcovington <doakcovington@protonmail.com>
2020-09-11 12:04:14 -0700

    Added route to home component

--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import EventsContainer from './EventsContainer';
 import TutorsContainer from './TutorsContainer';
+import Home from '../components/Home';
+import {Route} from 'react-router-dom';
 
 const HomeContainer = () => {
   return (
     <div>
+      <Route exact path='/' component={Home} />
       <EventsContainer />
       <TutorsContainer />
     </div>
