937662567320d4be631e901863fa96d4d4e7961c
doakcovington <doakcovington@protonmail.com>
2020-09-11 11:52:05 -0700

    Built out HomeContainer

---
+++ src/containers/HomeContainer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import EventsContainer from './EventsContainer';
+import TutorsContainer from './TutorsContainer';
+
+const HomeContainer = () => {
+  return (
+    <div>
+      <EventsContainer />
+      <TutorsContainer />
+    </div>
+  )
+}
+
+export default HomeContainer;
