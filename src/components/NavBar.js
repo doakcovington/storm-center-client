e298a309c2a8a8e22e743d7f2c6e3d94b39b9114
doakcovington <doakcovington@protonmail.com>
2020-09-14 15:50:36 -0700

    Added NavBar styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,30 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+
 
 const NavBar = () => {
   return (
     <div>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/events/">Events</NavLink>
-      <NavLink to="/events/new">Add Event</NavLink>
-      <NavLink to="/tutors/">Tutors</NavLink>
-      <NavLink to="/tutors/new">Add Tutor</NavLink>
+      <Navbar bg="dark" variant="dark">
+        <Nav className="mr-auto">
+          <Nav.Item>
+            <Nav.Link href="/">Home</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="link-1" href="/events/">Events</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="link-2" href="/events/new">Add Event</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="link-3" href="/tutors/">Tutors</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="link-4" href="/tutors/new">Add Tutor</Nav.Link>
+          </Nav.Item>
+        </Nav>
+      </Navbar>
     </div>
   );
 };
