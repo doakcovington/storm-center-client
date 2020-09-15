6fc51879783f4f666cc06aacefedea127e321d2d
doakcovington <doakcovington@protonmail.com>
2020-09-15 15:44:54 -0700

    formatted date

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import Card from 'react-bootstrap/Card'
+import Card from 'react-bootstrap/Card';
+import { format } from 'date-fns';
+
 
 const Event = (props) => {
 
   return (
     <Card style={{ width: '18rem' }} key={props.event.id}>
       <Card.Header as="h5">{props.event.attributes.name}</Card.Header>
-          <Card.Text>{props.event.attributes.date}</Card.Text>
+          <Card.Text>{format(new Date(props.event.attributes.date), 'MM/dd/yyyy')}</Card.Text>
           <Card.Text>{props.event.attributes.description}</Card.Text>
           <Card.Text>Tutor: {props.event.attributes.tutor.name}</Card.Text>
           <Card.Link href={props.event.attributes.location}>Zoom Link</Card.Link>
