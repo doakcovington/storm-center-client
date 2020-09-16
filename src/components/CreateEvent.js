8a1f43df3560b416a57522c5c67e275a7f7a1044
doakcovington <doakcovington@protonmail.com>
2020-09-15 18:40:54 -0700

    Added controlId to each group

--- src/components/CreateEvent.js
+++ src/components/CreateEvent.js
@@ -44,33 +44,33 @@ class CreateEvent extends React.Component {
     }
     return (
         <Form onSubmit={this.handleSubmit}>
-        <Form.Group>
+        <Form.Group controlId="formGroupEventName">
           <Form.Label column lg={2}>Event Name:</Form.Label>
           <input type='text' placeholder='Name' value={this.state.name} name='name' onChange={this.handleChange}/>
         </Form.Group>
-        <Form.Group>
+        <Form.Group controlId="formGroupDescription">
           <Form.Label column lg={2}>Event Description:</Form.Label>
           <input type='text' placeholder='Description' value={this.state.description} name='description' onChange={this.handleChange}/>
         </Form.Group>
-        <Form.Group>
+        <Form.Group controlId="formGroupDate">
           <Form.Label column lg={2}>Event Date:</Form.Label>
           <input type='date' name='date' value={this.state.date} onChange={this.handleChange}/>
         </Form.Group>
-        <Form.Group>
+        <Form.Group controlId="formGroupLocation">
           <Form.Label column lg={2}>Event Location/Link:</Form.Label>
           <input type='text' placeholder='url...' value={this.state.location} name='location' onChange={this.handleChange}/>
         </Form.Group>
-        <Form.Group>
+        <Form.Group controlId="formGroupSpaces">
           <Form.Label column lg={2}>Event Spaces:</Form.Label>
           <input type='number' placeholder='5' value={this.state.number} name='spaces' onChange={this.handleChange}/>
         </Form.Group>
-        <Form.Group>
-        <Form.Label column lg={2}>Event Tutor:</Form.Label>
-          <select id='tutor_id' name='tutor_id' value={this.state.tutor_id} onChange={this.handleChange}>
-            <option value='1'>Doak</option>
-            <option value='2'>Heidi</option>
-            <option value='3'>John</option>
-          </select>
+        <Form.Group controlId="formGroupTutor">
+          <Form.Label column lg={2}>Event Tutor:</Form.Label>
+            <select id='tutor_id' name='tutor_id' value={this.state.tutor_id} onChange={this.handleChange}>
+              <option value='1'>Doak</option>
+              <option value='2'>Heidi</option>
+              <option value='3'>John</option>
+            </select>
         </Form.Group>
           <Button variant="primary" onClick={this.handleSubmit}>Add Event</Button>
         </Form>
