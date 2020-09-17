f75601a4c30f5d847a521f0008baee46eaa97cb5
doakcovington <doakcovington@protonmail.com>
2020-09-17 15:55:50 -0700

    Added margin top to cards

--- src/components/Event.js
+++ src/components/Event.js
@@ -13,16 +13,18 @@ const Event = (props) => {
   }
 
   return (
-    <Card style={{ width: '18rem' }} key={props.event.id}>
-      <Card.Header as="h5">{props.event.attributes.name}</Card.Header>
-          <Card.Text>{format(new Date(props.event.attributes.date), 'MM/dd/yyyy')} at {props.event.attributes.time}</Card.Text>
-          <Card.Img variant="top" src={props.event.attributes.icon_url} />
-          <Card.Text>{props.event.attributes.description}</Card.Text>
-          <Card.Text>Tutor: {props.event.attributes.tutor.name}</Card.Text>
-          <Card.Link href={props.event.attributes.location}>Zoom Link</Card.Link>
-      <Card.Footer>Spaces: {props.event.attributes.spaces}</Card.Footer>
-      <Button variant="danger" onClick={() => handleDelete(props.event.id)}>Delete</Button>
+    <div className='mt-5'>
+      <Card style={{ width: '18rem' }} key={props.event.id}>
+        <Card.Header as="h5">{props.event.attributes.name}</Card.Header>
+            <Card.Text>{format(new Date(props.event.attributes.date), 'MM/dd/yyyy')} at {props.event.attributes.time}</Card.Text>
+            <Card.Img variant="top" src={props.event.attributes.icon_url} />
+            <Card.Text>{props.event.attributes.description}</Card.Text>
+            <Card.Text>Tutor: {props.event.attributes.tutor.name}</Card.Text>
+            <Card.Link href={props.event.attributes.location}>Zoom Link</Card.Link>
+        <Card.Footer>Spaces: {props.event.attributes.spaces}</Card.Footer>
+        <Button variant="danger" onClick={() => handleDelete(props.event.id)}>Delete</Button>
     </Card>
+    </div>
   )
 }
 
