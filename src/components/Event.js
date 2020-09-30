1b067e152b020f64cb10bb2f71409c6d240aa8d1
doakcovington <doakcovington@protonmail.com>
2020-09-30 12:41:53 -0700

    Added RSVP state and button

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,35 +1,42 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
 import { format } from 'date-fns';
-import {deleteEvent} from '../actions/deleteEvent';
 import Button from 'react-bootstrap/Button';
 import {connect} from 'react-redux';
 
-
-const Event = (props) => {
-  const handleDelete = (eventData) => {
-    props.deleteEvent(eventData) //event data sends the id
+class Event extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {rsvpStatus: "Have Not RSVP'D"}
   }
 
-  //checks tutor relationship
-  //If there is a tutor assigned to event then show tutor name, else display there is no tutor 
-  const tutorCard = props.event.attributes.tutor ? <Card.Text>Tutor: {props.event.attributes.tutor.name}</Card.Text> : <Card.Text>No Tutor is Assigned to This Event</Card.Text> 
-  
-  return (
-    <div className='mt-5'>
-      <Card style={{ width: '18rem' }} key={props.event.id}>
-        <Card.Header as="h5">{props.event.attributes.name}</Card.Header>
-            <Card.Text>{format(new Date(props.event.attributes.date), 'MM/dd/yyyy')} at {props.event.attributes.time}</Card.Text>
-            <Card.Img variant="top" src={props.event.attributes.icon_url} />
-            <Card.Text>{props.event.attributes.description}</Card.Text>
-            {tutorCard}
-            <Card.Link href={props.event.attributes.location}>Zoom Link</Card.Link>
-        <Card.Footer>Spaces: {props.event.attributes.spaces}</Card.Footer>
-        <Button variant="danger" onClick={() => handleDelete(props.event.id)}>Delete</Button>
-    </Card>
-    </div>
-  )
+  changeRsvpStatus = () => {
+    const newRsvpStatus = this.state.rsvpStatus = "Have RSVP'D";
+    this.setState({rsvpStatus: newRsvpStatus})
+  }
+
+  tutorCard = () => {
+    return this.props.event.attributes.tutor ? <Card.Text>Tutor: {this.props.event.attributes.tutor.name}</Card.Text> : <Card.Text>No Tutor Assigned to This Event</Card.Text>
+  }
+
+  render() {
+    return (
+      <div>
+        <Button onClick={this.changeRsvpStatus}>RSVP</Button>
+        <Card style={{ width: '18rem' }} key={this.props.event.id}>
+         <Card.Header as="h5">{this.props.event.attributes.name}</Card.Header>
+            <Card.Text>{format(new Date(this.props.event.attributes.date), 'MM/dd/yyyy')} at {this.props.event.attributes.time}</Card.Text>
+             <Card.Img variant="top" src={this.props.event.attributes.icon_url} />
+            <Card.Text>{this.props.event.attributes.description}</Card.Text>
+             {this.tutorCard}
+             <Card.Link href={this.props.event.attributes.location}>Zoom Link</Card.Link>
+          <Card.Text>Spaces: {this.props.event.attributes.spaces}</Card.Text>
+         <Card.Footer> Status: {this.state.rsvpStatus}</Card.Footer>
+     </Card>
+      </div>
+    )
+  }
 }
 
 
-export default connect(null, {deleteEvent})(Event);
+export default connect(null)(Event);
