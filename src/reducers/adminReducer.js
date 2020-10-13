5449f49d08822f827d3d2a359a284edeaedb232b
doakcovington <doakcovington@protonmail.com>
2020-10-12 19:24:54 -0700

    built out adminReducer

--- src/reducers/adminReducer.js
+++ src/reducers/adminReducer.js
@@ -1,9 +1,23 @@
 const defaultState = {
-
+  loggedIn: false,
+  admin: {}
 }
 
 const adminReducer = (state = defaultState, action) => {
-
+  switch(action.type) {
+    case "SET_ADMIN":
+      return {
+        loggedIn: true,
+        admin: {...action.payload}
+      }
+    case "LOG_OUT":
+      localStorage.clear()
+      return {
+        loggedIn: false,
+        admin: {}
+      }
+    default: return state
+  }
 }
 
 export default adminReducer;
