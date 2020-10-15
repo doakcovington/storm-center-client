fca568446b6d6e8793e9fb5a1685e919c83a3eb8
doakcovington <doakcovington@protonmail.com>
2020-10-14 18:59:33 -0700

    Created Login component

---
+++ src/components/Login.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import {fetchAdmin} from '../actions/adminActions';
+
+class Login extends React.Component {
+
+}
+
+export default connect(null,)(Login);
