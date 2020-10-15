10f436a212dad9b7d7fc0db92b9d9929c243a915
doakcovington <doakcovington@protonmail.com>
2020-10-14 19:02:39 -0700

    Built out login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,6 +4,24 @@ import {fetchAdmin} from '../actions/adminActions';
 
 class Login extends React.Component {
 
+  state = {
+    email: "",
+    password: "",
+  }
+
+  render() {
+    return(
+      <div>
+        <h2>Login:</h2>
+        <form>
+          <input type="text" name="email" placeholder="email" value={this.state.email} onChange={this.handleOnChange}></input>
+          <input type="password" name="password" placeholder="password" value={this.state.password} onChange={this.handleOnChange}></input>
+          <input type="submit" value="login"></input>
+        </form>
+      </div>
+    )
+  }
+
 }
 
 export default connect(null,)(Login);
