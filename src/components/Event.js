1756fdadaf0e4b89b79ac906bac8386310854431
doakcovington <doakcovington@protonmail.com>
2020-10-20 19:32:33 -0700

    Event is functional component

--- src/components/Event.js
+++ src/components/Event.js
@@ -3,27 +3,23 @@ import Card from 'react-bootstrap/Card';
 import { format } from 'date-fns';
 import {connect} from 'react-redux';
 
-class Event extends React.Component {
+const Event = (props) => {
+  
+  const tutorCard = props.event.attributes.tutor ? <Card.Text>Tutor: {props.event.attributes.tutor.name}</Card.Text> : <Card.Text>No Tutor is Assigned to This Event</Card.Text> 
 
-  tutorCard = () => {
-    return this.props.event.attributes.tutor ? <Card.Text>Tutor: {this.props.event.attributes.tutor.name}</Card.Text> : <Card.Text>No Tutor Assigned to This Event</Card.Text>
-  }
-
-  render() {
-    return (
-      <div>
-        <Card style={{ width: '18rem' }} key={this.props.event.id}>
-         <Card.Header as="h5">{this.props.event.attributes.name}</Card.Header>
-            <Card.Text>{format(new Date(this.props.event.attributes.date), 'MM/dd/yyyy')} at {this.props.event.attributes.time}</Card.Text>
-             <Card.Img variant="top" src={this.props.event.attributes.icon_url} />
-            <Card.Text>{this.props.event.attributes.description}</Card.Text>
-             {this.tutorCard}
-             <Card.Link href={this.props.event.attributes.location}>Zoom Link</Card.Link>
-          <Card.Text>Spaces: {this.props.event.attributes.spaces}</Card.Text>
-     </Card>
-      </div>
-    )
-  }
+  return (
+    <div className='mt-5'>
+      <Card style={{ width: '18rem' }} key={props.event.id}>
+        <Card.Header as="h5">{props.event.attributes.name}</Card.Header>
+            <Card.Text>{format(new Date(props.event.attributes.date), 'MM/dd/yyyy')} at {props.event.attributes.time}</Card.Text>
+            <Card.Img variant="top" src={props.event.attributes.icon_url} />
+            <Card.Text>{props.event.attributes.description}</Card.Text>
+            {tutorCard}
+            <Card.Link href={props.event.attributes.location}>Zoom Link</Card.Link>
+        <Card.Footer>Spaces: {props.event.attributes.spaces}</Card.Footer>
+      </Card>
+    </div>
+  )
 }
 
 
