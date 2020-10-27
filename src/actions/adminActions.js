ace81e6b9549ce16eefacacd3efcdb51df032127
doakcovington <doakcovington@protonmail.com>
2020-10-26 20:25:47 -0700

    Fixed bug

--- src/actions/adminActions.js
+++ src/actions/adminActions.js
@@ -4,16 +4,16 @@ export const logAdminOut = () => ({type: "LOG_OUT"})
 
 export const fetchAdmin = (adminInfo) => dispatch => {
   fetch(`http://localhost:5000/api/v1/login`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "applications/json",
-      "Accept": "application/json"
-    },
-    body: JSON.stringify(adminInfo)
+      method: "POST",
+      headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+      },
+      body: JSON.stringify(adminInfo)
   })
   .then(res => res.json())
   .then(data => {
-    localStorage.setItem("token", data.token)
-    dispatch(setAdmin(data.admin))
+      localStorage.setItem("token", data.token)
+      dispatch(setAdmin(data.admin))
   })
 }
