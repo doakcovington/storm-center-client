06929185bc06a80be0080c0f6c81c32c3037714e
doakcovington <doakcovington@protonmail.com>
2020-11-22 19:46:49 -0800

    Removed admin loggin

--- src/App.js
+++ src/App.js
@@ -3,34 +3,22 @@ import './App.css';
 import {connect} from 'react-redux';
 import NavBar from './components/NavBar';
 import Login from './components/Login';
-import {logAdminOut} from './actions/adminActions';
 
 //containers
 import Home from './components/Home';
 
 class App extends React.Component {
 
-  handleOnClick = () => {
-    if (this.props.adminReducer.loggedIn) {
-      this.props.logAdminOut();
-    }
-  }
 
   render () {
     return (
       <div className="App">
-        {!this.props.adminReducer.loggedIn ? <Login /> :  <Home />}
         <NavBar />
       </div>
     );
   }
 }
 
-const mapStateToProps = (state) => {
-  return {
-    adminReducer: state.adminReducer
-  }
-}
 
 //connect() connects component to redux store
-export default connect(mapStateToProps, { logAdminOut })(App);
+export default connect(null)(App);
